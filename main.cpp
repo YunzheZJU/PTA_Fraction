49cf1f56bd82f18b06894c88d044ce80a199eb71
Yunzhe <iLoveLive@honoka.moe>
2018-01-05 20:37:05 +0800

    Done.

--- main.cpp
+++ main.cpp
@@ -12,15 +12,9 @@ private:
 public:
     Fraction() : upper(0), lower(0) {};
 
-    Fraction(double num) {
-
-    }
+    Fraction(double num);
 
     Fraction(long long upper, long long lower) : upper(upper), lower(lower) {}
-
-    void print() {
-        cout << "I am " << upper << "/" << lower << endl;
-    }
 
     long long calMaxFactor(long long num_1, long long num_2);
 
@@ -39,7 +33,17 @@ public:
     friend const Fraction operator/(const Fraction &fraction_1, const Fraction &fraction_2);
 
     friend ostream &operator<<(ostream &os, const Fraction &fraction);
+
+    explicit operator double() const;
 };
+
+Fraction::Fraction(double num) {
+    long long upper = num * 10000000000000000 + 0.5;    // Fuck 0.5
+    long long lower = 10000000000000000LL;
+    Fraction fraction = Fraction(upper, lower);
+    fraction.reduce();
+    *this = fraction;
+}
 
 long long Fraction::calMaxFactor(long long num_1, long long num_2) {
     long long th = num_1, tl = num_2;
@@ -54,14 +58,14 @@ long long Fraction::calMaxFactor(long long num_1, long long num_2) {
 }
 
 void Fraction::reduce() {
-    cout << "I am " << upper << "/" << lower << endl;
+//    cout << "I am " << upper << "/" << lower << endl;
     // Reduce myself
     long long maxFactor = calMaxFactor(this->upper, this->lower);
     if (maxFactor > 1) {
         this->upper /= maxFactor;
         this->lower /= maxFactor;
     }
-    cout << "After reduction, I am " << upper << "/" << lower << endl;
+//    cout << "After reduction, I am " << upper << "/" << lower << endl;
 }
 
 const Fraction operator+(const Fraction &fraction_1, const Fraction &fraction_2) {
@@ -73,15 +77,27 @@ const Fraction operator+(const Fraction &fraction_1, const Fraction &fraction_2)
 }
 
 const Fraction operator-(const Fraction &fraction_1, const Fraction &fraction_2) {
-
+    long long upper = fraction_1.upper * fraction_2.lower - fraction_2.upper * fraction_1.lower;
+    long long lower = fraction_1.lower * fraction_2.lower;
+    Fraction newFraction(upper, lower);
+    newFraction.reduce();
+    return newFraction;
 }
 
 const Fraction operator*(const Fraction &fraction_1, const Fraction &fraction_2) {
-
+    long long upper = fraction_1.upper *  fraction_2.upper;
+    long long lower = fraction_1.lower * fraction_2.lower;
+    Fraction newFraction(upper, lower);
+    newFraction.reduce();
+    return newFraction;
 }
 
 const Fraction operator/(const Fraction &fraction_1, const Fraction &fraction_2) {
-
+    long long upper = fraction_1.upper *  fraction_2.lower;
+    long long lower = fraction_1.lower * fraction_2.upper;
+    Fraction newFraction(upper, lower);
+    newFraction.reduce();
+    return newFraction;
 }
 
 istream &operator>>(istream &is, Fraction &fraction) {
@@ -97,35 +113,46 @@ istream &operator>>(istream &is, Fraction &fraction) {
 }
 
 ostream &operator<<(ostream &os, const Fraction &fraction) {
-    os << fraction.upper << "/" << fraction.lower << endl;
+    os << fraction.upper << "/" << fraction.lower;
     return os;
+}
+
+Fraction::operator double() const {
+    return this->upper / (double) this->lower;
 }
 
 int main() {
     Fraction f1;
     Fraction f2;
     cin >> f1 >> f2;
-    f1.print();
-    f2.print();
-    cout << "f1 + f2 = " << f1 + f2 << endl;
+//    cout << "f1 + f2 = " << f1 + f2 << endl;
+//    cout << "double(f1 + f2) = " << double(f1 + f2) << endl;
+//    cout << "f1 - f2 = " << f1 - f2 << endl;
+//    cout << "f1 * f2 = " << f1 * f2 << endl;
+//    cout << "f1 / f2 = " << f2 / f1 << endl;
 //    Fraction f1;
 //    Fraction f2;
 //    cin >> f1 >> f2;
-//    cout << f1 + f2 << endl;
-//    cout << (double) (f1 + f2) << endl;
-//    cout << f1 - f2 << endl;
-//    cout << f1 * f2 << endl;
-//    cout << f2 / f1 << endl;
-//    double d;
-//    cin >> d;
-//    Fraction f(d);
-//    cout << f << endl;
-//    f = 0.5;
-//    cout << f << endl;
-//    Fraction *p = &f1;
-//    f1 = *p;
-//    cout << f1 << endl;
-//    cin >> f1;
-//    f1 = f1 * Fraction(2.0);
-//    cout << f1 << endl;
+    cout << f1 + f2 << endl;
+    cout << (double) (f1 + f2) << endl;
+    cout << f1 - f2 << endl;
+    cout << f1 * f2 << endl;
+    cout << f2 / f1 << endl;
+    double d;
+    cin >> d;
+//    cout << "d = " << d << endl;
+    Fraction f(d);
+//    cout << "f = " << f << endl;
+    cout << f << endl;
+    f = 0.5;
+//    cout << "f = " << f << endl;
+    cout << f << endl;
+    Fraction *p = &f1;
+    f1 = *p;
+//    cout << "f1 = " << f1 << endl;
+    cout << f1 << endl;
+    cin >> f1;
+    f1 = f1 * Fraction(2.0);
+//    cout << "f1 = " << f1 << endl;
+    cout << f1 << endl;
 }
